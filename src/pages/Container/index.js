0abd8e832375700d33554af49b872b8ba9d2863a
Daniela <feitosaferreiradaniela@gmail.com>
2022-04-06 09:09:45 -0400

    feat 3

--- src/pages/Container/index.js
+++ src/pages/Container/index.js
@@ -6,16 +6,26 @@ import { Link } from "react-router-dom";
 export default function Container() {
   return (
     <div className="container">
-      <div className="containers__list">
+            <div className="containers__list">
+          <div className="containers__labels">
+            <label>Empresa: CAPACITORES A</label>
+            <label>São Paulo - SP    CNPJ: 03.709.814/0064-71.</label>
+            <label>Motorista: José Alves</label>
+            <label>Placa: 5487</label>
+          </div>
+
+          <h1 className="timer">
+            TIMER
+          </h1>
         <div className="containers__button">
           <Button variant="contained">EDITAR</Button>
           <Button 
             style={{
               backgroundColor: "#4A8360",
-              }}
-              component={Link} to="containers" 
+            }}
+            component={Link} to="containers" 
               className="btn-primary" variant="contained">FOLLOW UP</Button>  
-        </div>
+            </div>
       </div>
     </div>
   );
