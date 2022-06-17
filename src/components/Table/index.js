1b5f6800f55883e64437073a3aab53fe9cff1595
Daniela <feitosaferreiradaniela@gmail.com>
2022-06-16 21:10:53 -0400

    up

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,15 +1,12 @@
 import './index.css';
-import { DataGrid, GridToolbarExport, GridToolbarContainer, GridToolbarExportContainer, ptBR, GridToolbarFilterButton, GridLinkOperator } from '@mui/x-data-grid';
+import { DataGrid, GridToolbarExport, GridToolbarContainer, ptBR, GridToolbarFilterButton, GridLinkOperator, GridToolbar } from '@mui/x-data-grid';
 import { renderConTableButton, renderTableButton } from '../TableButton'
 import { useDemoData } from '@mui/x-data-grid-generator';
 import { Button } from '@mui/material';
 import SearchInput from '../SearchInput';
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-function escapeRegExp(value) {
-    return value.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
-  }
 const columnsFollowUps = [
     { field: 'model', headerName: 'Modelo', width: 130 },
     { field: 'project', headerName: 'Projeto', width: 150 },
@@ -29,32 +26,30 @@ const columnsContainers = [
 ];
 
 const columnsInvoices = [
-    { field: 'Invoice', headerName: 'Container RF-ID', width: 180 },
-    { field: 'Invoice Date', headerName: 'Empresa', width: 100 },
-    { field: 'Payment Terms', headerName: 'Hora', width: 150 },
-    { field: 'Invoice Amount', headerName: 'Total de palates', width: 130 },
+    { field: 'invoice', headerName: 'Invoce', width: 180 },
+    { field: 'invoiceDate', headerName: 'Invoice Date', width: 100 },
+    { field: 'paymentTerms', headerName: 'Payment Terms', width: 150 },
+    { field: 'invoiceAmount', headerName: 'Invoice Amount', width: 130 },
     { field: 'action', headerName: 'Ação', width: 100, renderCell: renderConTableButton, filterable: false },
 ];
 
+function getFields(struct) {
+    return struct.map((element) => element.field );
+}
 
 export default function Table(props) {
-    const [searchText, setSearchText] = useState('');
     const [open, setOpen] = useState(false);
-    
+
     const { type, rows } = props;
     const [rowss, setRows] = useState(rows);
-    const requestSearch = (searchValue) => {
-        setSearchText(searchValue);
-        const searchRegex = new RegExp(escapeRegExp(searchValue), 'i');
-        const filteredRows = data.rows.filter((row) => {
-          return Object.keys(row).some((field) => {
-            return searchRegex.test(row[field].toString());
-          });
-        });
-        setRows(filteredRows);
-      };
+    const [columns, setColumns] = useState(rows);
+
+    useEffect(() => {
+        columnHeader()
+    }, []);
+
     const renderButton = () => {
-        if(type == "follow-ups") {
+        if (type == "follow-ups") {
             return (
                 <Button component={Link} to={`1`} variant="contained">Cadastrar</Button>
             )
@@ -64,20 +59,20 @@ export default function Table(props) {
             )
         }
     }
-    
+
     function CustomToolbar() {
         return (
             <GridToolbarContainer>
-                 <div className="table__top">
-                    {renderButton()} 
+                <div className="table__top">
+                    {renderButton()}
                     <SearchInput onChange={props.onChange} label={"CONSULTAR"} className="top__search" />
-                <GridToolbarFilterButton variant="contained"/>
-                <GridToolbarExport variant="contained" columns={[{ field: 'action', disableExport: true }]} printOptions={{ disableToolbarButton: true }} />
+                    <GridToolbarFilterButton variant="contained" />
+                    <GridToolbarExport variant="contained" columns={[{ field: 'action', disableExport: true }]} printOptions={{ disableToolbarButton: true }} />
                 </div>
             </GridToolbarContainer>
         );
     }
-    
+
     const { data } = useDemoData({
         dataSet: 'Flex',
         rowLength: 100,
@@ -85,15 +80,15 @@ export default function Table(props) {
 
     function columnHeader() {
         if (type == "follow-ups") {
-            return columnsFollowUps;
+            setColumns(columnsFollowUps);
         }
 
         if (type == "containers") {
-            return columnsContainers;
+            setColumns(columnsContainers);
         }
 
         if (type == "invoices") {
-            return columnsInvoices;
+            setColumns(columnsInvoices);
         }
     }
     return (
@@ -102,54 +97,14 @@ export default function Table(props) {
                 <DataGrid
                     localeText={ptBR.components.MuiDataGrid.defaultProps.localeText}
                     rows={rowss}
-                    components={{ Toolbar: CustomToolbar, }}
-                    columns={columnHeader()}
+                    components={{ Toolbar: CustomToolbar }}
+                    columns={columns}
                     pageSize={5}
                     rowsPerPageOptions={[5]}
                     checkboxSelection
                     componentsProps={{
-                        value: searchText,
-                        onChange: (event) => requestSearch(event.target.value),
-                        filterPanel: {
-                            clearSearch: () => requestSearch(''),
-                            // Force usage of "And" operator
-                            linkOperators: [GridLinkOperator.And],
-                            // Display columns by ascending alphabetical order
-                            columnsSort: 'asc',
-                            filterFormProps: {
-                                linkOperatorInputProps: {
-                                    variant: 'outlined',
-                                    size: 'small',
-                                },
-                                columnInputProps: {
-                                    variant: 'outlined',
-                                    size: 'small',
-                                    sx: { mt: 'auto' },
-                                },
-                                operatorInputProps: {
-                                    variant: 'outlined',
-                                    size: 'small',
-                                    sx: { mt: 'auto' },
-                                },
-                                deleteIconProps: {
-                                    sx: {
-                                        '& .MuiSvgIcon-root': { color: '#d32f2f' },
-                                    },
-                                },
-                            },
-                            sx: {
-                                // Customize inputs using css selectors
-                                '& .MuiDataGrid-filterForm': { p: 2 },
-                                '& .MuiDataGrid-filterForm:nth-child(even)': {
-                                    backgroundColor: (theme) =>
-                                        theme.palette.mode === 'dark' ? '#444' : '#f5f5f5',
-                                },
-                                '& .MuiDataGrid-filterFormLinkOperatorInput': { mr: 2 },
-                                '& .MuiDataGrid-filterFormColumnInput': { mr: 2, width: 150 },
-                                '& .MuiDataGrid-filterFormOperatorInput': { mr: 2 },
-                                '& .MuiDataGrid-filterFormValueInput': { width: 200 },
-                            },
-                        },
+                        showQuickFilter: true,
+                        quickFilterProps: { debounceMs: 500 },
                     }}
                 />
             </div>
--- src/pages/Despachante/index.js
+++ src/pages/Despachante/index.js
@@ -4,10 +4,10 @@ import './index.css';
 export default function Despachante() {
   return (
     <div className="pagrec">
-    <Input className="pagrec__input" type="text" placeholder="" label="Modelo" />
-    <Input className="pagrec__input" type="text" placeholder="" label="Customer" />
-    <Input className="pagrec__input" type="text" placeholder="" label="Projeto" />
-    <Input className="pagrec__input" type="text" placeholder="" label="Produto" />
+    <Input className="pagrec__input" type="text" placeholder="" label="Channel" />
+    <Input className="pagrec__input" type="text" placeholder="" label="DI NUMBER" />
+    <Input className="pagrec__input" type="text" placeholder="" label="Recinto" />
+    <Input className="pagrec__input" type="text" placeholder="" label="Per" />
     <Input className="pagrec__input" type="text" placeholder="" label="VALOR PEDIDO ORIGINAL" />
     <Input className="pagrec__input" type="text" placeholder="" label="PO (Referência)" />
     <Input className="pagrec__input" type="text" placeholder="" label="LOTE / PAG REC PROTHEUS" />
